7971df5c351cab1ea7b57edff9addf545f64ebba
uFeat <98409133+Amne-Dev@users.noreply.github.com>
2025-01-27 19:56:22 +0100

    Projects Page final phase
    - Added 14 Student Project To the theoretical Side of the website

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,21 +1,46 @@
 const projects = {
     theory: [
-        { title: "Physics Concepts", link: "#", name: "Student A", image: "https://placehold.co/150" },
-        { title: "Mathematical Theories", link: "#", name: "Student B", image: "https://placehold.co/150" }
+        { title: "A new Vision for Designing Student's Time-Table at School", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Aya%20Hammi%20Mly%20ismail.docx", name: "Aya Hammi", image: "https://placehold.co/150" },
+        { title: "A New Vision for Fighting Bullying at School", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Boufaris%20Sara%20Mly%20ismail.docx", name: "Boufaris Sara", image: "https://placehold.co/150" },
+        { title: "A new vision to convert the student's footsteps at school into electricity", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Boulangas%20Kaoutar%20Mly%20ismail.docx", name: "Boulangas Kaoutar", image: "https://placehold.co/150" },
+        { title: "Student's Engagement in Community Service Projects", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Elbaraka%20Fares%20Mly%20ismail.docx", name: "Elbaraka Fares", image: "https://placehold.co/150" },
+        { title: "Quality of Education in Morocco: Between theory and practice", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Fatima%20Zahra%20Ben%20Daouia%20Mly%20ismail.docx", name: "Fatima Zahra Ben Daouia", image: "https://placehold.co/150" },
+        { title: "A New Vision for Sustainable Education", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Firdaws%20Idrissi%20Mly%20ismail.docx", name: "Firdaws Idrissi", image: "https://placehold.co/150" },
+        { title: "Energy Storage Solutions: Electromagnetism for Developing Efficient and Sustainable Battery Technologies", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Hassan%20Ihyaoui%20Mly%20Ismail.docx", name: "Hassan Ihyaoui", image: "https://placehold.co/150" },
+        { title: "A New Vision to the Teaching and Learning Process of Math", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/imane%20zaghdoud%20Mly%20Ismail.docx", name: "Imane Zaghdoud", image: "https://placehold.co/150" },
+        { title: "The Moroccan Education System Between Theory and Practice", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Malak%20Ben%20Sghir%20Mly%20ismail.docx", name: "Malak Ben Sghir", image: "https://placehold.co/150" },
+        { title: "Gender Equality and Equity in Children's Education", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Meryeme%20Essaissi%20Mly%20ismail.docx", name: "Meryeme Essaissi", image: "https://placehold.co/150" },
+        { title: "A New Vision to Save Life Below Water", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Nour%20ElHouda%20Boukhita%20Mly%20Ismail.docx", name: "Nour ElHouda Boukhita", image: "https://placehold.co/150" },
+        { title: "Children's Education in Case Divorce in Morocco", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Oumaima%20Lakhlif%20Mly%20ismail.docx", name: "Oumaima Lakhlif", image: "https://placehold.co/150" },
+        { title: "A New Vision for Saving Water Sources in the Region of Fes Meknes ", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Rahmani%20Mohamed%20Karim%20Mly%20ismail.docx", name: "Rahmani Mohamed Karim", image: "https://placehold.co/150" },
+        { title: "Taking Individual Actions for Zero Hunger", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Walid%20Mzoughi%20Mly%20Ismail.docx", name: "Walid Mzoughi", image: "https://placehold.co/150" },
+
     ],
     practice: [
-        { title: "Robotics Project", link: "#", name: "Student C", image: "https://placehold.co/150" },
-        { title: "Chemistry Lab Work", link: "#", name: "Student D", image: "https://placehold.co/150" }
+        { title: "Comings Soon.", link: "#", name: "Soon...", image: "https://placehold.co/150" },
+
     ]
 };
 
 const projectsAr = {
     theory: [
-        { title: "مفاهيم الفيزياء", link: "#", name: "الطالب أ", image: "https://placehold.co/150" },
-        { title: "نظريات رياضية", link: "#", name: "الطالب ب", image: "https://placehold.co/150" }
+        { title: "A new Vision for Designing Student's Time-Table at School", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Aya%20Hammi%20Mly%20ismail.docx", name: "Aya Hammi", image: "https://placehold.co/150" },
+        { title: "A New Vision for Fighting Bullying at School", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Boufaris%20Sara%20Mly%20ismail.docx", name: "Boufaris Sara", image: "https://placehold.co/150" },
+        { title: "A new vision to convert the student's footsteps at school into electricity", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Boulangas%20Kaoutar%20Mly%20ismail.docx", name: "Boulangas Kaoutar", image: "https://placehold.co/150" },
+        { title: "Student's Engagement in Community Service Projects", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Elbaraka%20Fares%20Mly%20ismail.docx", name: "Elbaraka Fares", image: "https://placehold.co/150" },
+        { title: "Quality of Education in Morocco: Between theory and practice", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Fatima%20Zahra%20Ben%20Daouia%20Mly%20ismail.docx", name: "Fatima Zahra Ben Daouia", image: "https://placehold.co/150" },
+        { title: "A New Vision for Sustainable Education", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Firdaws%20Idrissi%20Mly%20ismail.docx", name: "Firdaws Idrissi", image: "https://placehold.co/150" },
+        { title: "Energy Storage Solutions: Electromagnetism for Developing Efficient and Sustainable Battery Technologies", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Hassan%20Ihyaoui%20Mly%20Ismail.docx", name: "Hassan Ihyaoui", image: "https://placehold.co/150" },
+        { title: "A New Vision to the Teaching and Learning Process of Math", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/imane%20zaghdoud%20Mly%20Ismail.docx", name: "Imane Zaghdoud", image: "https://placehold.co/150" },
+        { title: "The Moroccan Education System Between Theory and Practice", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Malak%20Ben%20Sghir%20Mly%20ismail.docx", name: "Malak Ben Sghir", image: "https://placehold.co/150" },
+        { title: "Gender Equality and Equity in Children's Education", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Meryeme%20Essaissi%20Mly%20ismail.docx", name: "Meryeme Essaissi", image: "https://placehold.co/150" },
+        { title: "A New Vision to Save Life Below Water", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Nour%20ElHouda%20Boukhita%20Mly%20Ismail.docx", name: "Nour ElHouda Boukhita", image: "https://placehold.co/150" },
+        { title: "Children's Education in Case Divorce in Morocco", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Oumaima%20Lakhlif%20Mly%20ismail.docx", name: "Oumaima Lakhlif", image: "https://placehold.co/150" },
+        { title: "A New Vision for Saving Water Sources in the Region of Fes Meknes ", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Rahmani%20Mohamed%20Karim%20Mly%20ismail.docx", name: "Rahmani Mohamed Karim", image: "https://placehold.co/150" },
+        { title: "Taking Individual Actions for Zero Hunger", link: "https://github.com/Amne-Dev/MyIsmail-Web/raw/refs/heads/main/Projects/projectworks%20of%20students/Walid%20Mzoughi%20Mly%20Ismail.docx", name: "Walid Mzoughi", image: "https://placehold.co/150" },
+
     ],
     practice: [
-        { title: "مشروع الروبوتات", link: "#", name: "الطالب ج", image: "https://placehold.co/150" },
-        { title: "عمل مختبر الكيمياء", link: "#", name: "الطالب د", image: "https://placehold.co/150" }
+        { title: "Comings Soon.", link: "#", name: "Soon...", image: "https://placehold.co/150" },
     ]
 };
